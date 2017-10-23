48a0a103889977a28e81ccbaf9353c15812b77c3
Rachel-lichun <2325783404@qq.com>
2017-10-23 15:43:06 +0800

    check & switch

--- CheckElement.java
+++ CheckElement.java
@@ -22,9 +22,7 @@ public class CheckElement {
 		
 		driver.get("http://baidu.com");
 		driver.manage().window().maximize();
-		/*
-		 * 一.检查元素文本
-		 */
+//		一.检查元素文本
 		// 第一步：获得元素
 		WebElement element = driver.findElement(By.id("setf"));
 	   // 第二步：得到元素文本
@@ -38,25 +36,21 @@ public class CheckElement {
 		System.out.println("before");
 		assertEquals("fail", "把百度设为主页", text);
 		System.out.println("after");
-
-		/*
-		 * 二.检查元素的属性值（创建测试类验证元素属性是否正确）
-		 */
 		
+		
+//		二.检查元素的属性值（创建测试类验证元素属性是否正确）
 		/*
 		 * 通过title识别处理切换窗口
 		 * 用JS打开一个新窗口，当前窗口是百度页面，需要切换到新窗口，输入新的网址
 		 * 本次通过handle和title结合来切换窗口
-		 * 
 		 */
 		// 打开一个新窗口
 		JavascriptExecutor executor = (JavascriptExecutor) driver;
 		executor.executeScript("window.open();");
-		//父窗口
-//		String parentWinId = driver.getWindowHandle();
 		//获取所有窗口的句柄
 		Set<String> allId=driver.getWindowHandles();
-		//遍历，切换到新窗口
+		//切换窗口目前接触到有3种方法
+		//方法1：通过title识别处理切换窗口
 		for (String id : allId) {
 			if (driver.switchTo().window(id).getTitle().equals("新标签页")) {
 				driver.switchTo().window(id);
@@ -64,10 +58,19 @@ public class CheckElement {
 			}
 			
 		}
+		//方法2： 通过网页内容切换窗口
 		
-		// 定位到新窗口
+//		for (String id : allId) {
+//			if (driver.getPageSource().contains("")) {
+//				driver.switchTo().window(id);
+//			}
+//		}
+		
+		//方法3：通过窗口的name属性
 		//switchTo().window() ,需要知道窗口的name属性。在此，此方法不适合
 //		driver.switchTo().window("name");
+		
+		
 		driver.get("http://www.ziroom.com");
 		System.out.println("driver.getTitle():" + driver.getTitle());
 		driver.manage().window().maximize();
